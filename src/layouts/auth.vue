5e42085bebb6a0032d25423c8685c56a04d0287a
Abdullah Barakat <abdallah771363885@gmail.com>
2024-07-11 22:42:22 +0300

    feat: add auth layout

--- src/layouts/auth.vue
+++ src/layouts/auth.vue
@@ -1,3 +1,8 @@
 <template>
-    auth
+    <div class="w-screen h-screen overflow-hidden bg-amber">
+        
+    </div>
+
+
+    <RouterView/>
 </template>
--- src/typed-router.d.ts
+++ src/typed-router.d.ts
@@ -19,5 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/auth/login/': RouteRecordInfo<'/auth/login/', '/auth/login', Record<never, never>, Record<never, never>>,
+    '/auth/register/': RouteRecordInfo<'/auth/register/', '/auth/register', Record<never, never>, Record<never, never>>,
   }
 }
