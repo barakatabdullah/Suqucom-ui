7f05963d4c894828c49fea82bc4764b54fd2faa3
Barakat <abdallah771363885@gmail.com>
2024-08-03 22:30:52 +0300

    feat: add auth middleware

---
+++ src/stores/user.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { useRouter } from 'vue-router'
+import api from '@/config/axios'
+
+const router = useRouter()
+
+export const useUserStore = defineStore('User', {
+  state: () => {
+    return {
+      user: {
+        name: localStorage.getItem('user'),
+        role: localStorage.getItem('role'),
+        token: localStorage.getItem('token')
+      }
+    }
+  },
+  actions: {
+    // logout() {
+    //   api
+    //     .get(`/user/logout`, {
+    //       headers: {
+    //         Authorization: `Bearer ${this.user.token}`,
+    //         'Content-Type': 'application/json'
+    //       }
+    //     })
+    //     .then((res) => {
+    //       // eslint-disable-next-line eqeqeq
+    //       if (res.status == 200) {
+    //         localStorage.removeItem('token')
+    //         this.user.token = localStorage.getItem('token')
+    //         // router.push({ name: 'auth' })
+    //       }
+    //     })
+    //     .catch(() => {
+    //       localStorage.removeItem('token')
+    //       this.user.token = localStorage.getItem('token')
+    //     })
+    // },
+
+  }
+})
